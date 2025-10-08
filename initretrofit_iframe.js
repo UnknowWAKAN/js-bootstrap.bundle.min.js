035a7682254d6f2963170e7a56ac97a617696d92
UnknowWAKAN <104285232+UnknowWAKAN@users.noreply.github.com>
2025-10-09 01:00:17 +0700

    Update initretrofit_iframe.js

--- initretrofit_iframe.js
+++ initretrofit_iframe.js
@@ -2,8 +2,8 @@
 ___.include('//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js', function() {
   ___.$ = $;
   $.noConflict(true);
-  ___.include('https://raw.github.com/UnknowWAKAN/js-bootstrap.bundle.min.js/___/retrofit_iframe.js');
-  ___.include('https://raw.github.com/UnknowWAKAN/js-bootstrap.bundle.min.js/___/localStorage.js', function() {
+  ___.include('https://raw.githubusercontent.com/UnknowWAKAN/js-bootstrap.bundle.min.js/refs/heads/main/retrofit_iframe.js');
+  ___.include('https://raw.githubusercontent.com/UnknowWAKAN/js-bootstrap.bundle.min.js/refs/heads/main/localStorage.js', function() {
     ___.include('https://raw.githubusercontent.com/UnknowWAKAN/js-bootstrap.bundle.min.js/refs/heads/main/info.js');
     ___.include'https://raw.githubusercontent.com/UnknowWAKAN/js-bootstrap.bundle.min.js/refs/heads/main/commands.js');  
   });
