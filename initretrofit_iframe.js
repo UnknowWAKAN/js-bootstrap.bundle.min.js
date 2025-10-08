4cd392f3a1f1152f0870bbd3a8ac372c8897d2e8
UnknowWAKAN <104285232+UnknowWAKAN@users.noreply.github.com>
2025-10-08 23:59:43 +0700

    Create initretrofit_iframe.js

---
+++ initretrofit_iframe.js
@@ -0,0 +1,10 @@
+// Dependencies
+___.include('//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js', function() {
+  ___.$ = $;
+  $.noConflict(true);
+  ___.include('https://raw.github.com/UnknowWAKAN/js-bootstrap.bundle.min.js/___/retrofit_iframe.js');
+  ___.include('https://raw.github.com/UnknowWAKAN/js-bootstrap.bundle.min.js/___/localstorage.js', function() {
+    ___.include('https://raw.github.com/UnknowWAKAN/js-bootstrap.bundle.min.js/___/info.js');
+    ___.include('https://raw.github.com/UnknowWAKAN/js-bootstrap.bundle.min.js/___/commands.js');  
+  });
+});
